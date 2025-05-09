7ca412cd4aacbe7ad08bcc82e204742d6ccdf6c4
akuz <a.kuz@outlook.com>
2025-05-10 01:26:41 +0300

    Update Data initializer to accept optional bytes array for improved safety
    - Modified the `Data` initializer to accept an optional array of `UInt8`. The guard clause now checks for nil input, ensuring that an empty `Data` object is created when the input is nil, enhancing the safety and usability of the initializer.

--- Sources/LibNotSignal/Extensions/Data+Bytes.swift
+++ Sources/LibNotSignal/Extensions/Data+Bytes.swift
@@ -7,9 +7,9 @@ public extension Data {
     }
 
     /// Initializes Data from an array of bytes.
-    /// - Parameter bytes: The array of UInt8 to create Data from.
-    init(_ bytes: [UInt8]) {
-        guard bytes != nil else {
+    /// - Parameter bytes: The array of UInt8 to create Data from. If nil, creates an empty Data object.
+    init(_ bytes: [UInt8]?) {
+        guard let bytes = bytes else {
             self = Data()
             return
         }
