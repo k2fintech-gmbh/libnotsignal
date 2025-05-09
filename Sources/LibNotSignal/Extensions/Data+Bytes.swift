ff52636ca26c72ee1aa65b192370e8fa62e3be35
akuz <a.kuz@outlook.com>
2025-05-10 00:35:47 +0300

    Add Data extension for byte array conversion
    - Introduced a new extension for the `Data` type that provides a computed property `bytes` to retrieve an array of bytes and an initializer to create `Data` from a byte array. This enhancement simplifies the conversion between `Data` and byte arrays, improving usability in key management and serialization tasks.

---
+++ Sources/LibNotSignal/Extensions/Data+Bytes.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+public extension Data {
+    /// Returns an array of bytes from the Data object.
+    var bytes: [UInt8] {
+        return [UInt8](self)
+    }
+
+    /// Initializes Data from an array of bytes.
+    /// - Parameter bytes: The array of UInt8 to create Data from.
+    init(_ bytes: [UInt8]) {
+        self = Data(bytes)
+    }
+} 
